0eae3b965aad0cf453ad7afad5c3e885188793aa
makoge boris <makogeboris@gmail.com>
2024-05-01 16:42:34 +0100

    fix background overlay on mobile menu

--- script.js
+++ script.js
@@ -2,26 +2,20 @@
 
 const btn = document.getElementById("menu-btn");
 const menu = document.querySelector(".mobile-menu");
-const hideElements = document.querySelectorAll(".hide");
+const overlay = document.querySelector(".overlay");
 
-// Initially remove 'open' class from button, 'hidden' class from menu, and 'hide' class from hidden elements
 btn.classList.remove("open");
 menu.classList.remove("hidden");
-hideElements.forEach((hide) => {
-  hide.classList.remove("hide");
-});
+overlay.classList.add("hide");
 
 btn.addEventListener("click", navToggle);
 
 function navToggle() {
   btn.classList.toggle("open");
   menu.classList.toggle("hidden");
+  overlay.classList.toggle("hide");
 
   // Update the aria-expanded attribute of the menu button based on the menu's visibility
   const isMenuHidden = menu.classList.contains("hidden");
   btn.setAttribute("aria-expanded", isMenuHidden ? "false" : "true");
-
-  hideElements.forEach((hide) => {
-    hide.classList.toggle("hide");
-  });
 }
